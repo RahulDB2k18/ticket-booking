f39393d2591ed0de0866b83b14e98ea0975c3fea
RahulDB2k18 <rahuldb2k18@gmail.com>
2023-06-20 13:09:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+// import Forgot from './components/Forgot';
+import Home from './components/Home';
+import Protect from './components/Protect';
+// import Seats from './components/Seats';
+import Profile from './components/Profile';
+import Busdetail from './components/Busdetail';
+import Bookbus from './components/Bookbus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Signup/>} ></Route>
+          <Route path='/login' element={<Login/>} ></Route>
+          {/* <Route path='/forgot-password' element={<Forgot/>} ></Route> */}
+          <Route path='/home' element={ <Protect Child={Home}/>  }/>
+          <Route path='/profile' element={ <Protect Child={Profile}/>  }/>
+          {/* <Route path='/bookseat' element={<Seats/>} ></Route> */}
+          <Route path='/busdetail/:busid' element={ <Protect Child={Busdetail}/>  }/>
+          <Route path='/bookbus' element={ <Protect Child={Bookbus}/>  }/>
+          <Route path='/bus' element={ <Bookbus/> }/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
